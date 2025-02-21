4b34a547b27d50a5abfc2b5b276418fc8323eedb
nikiiigoyal <nikiiigoyal@gmail.com>
2025-02-21 18:18:32 +0530

    Add background and border for whole

---
+++ src/components/layout/index.jsx
@@ -0,0 +1,10 @@
+function Layout() {
+  return (
+    <div
+      className="min-h-screen w-screen bg-[#1c1c28] bg-[url('/gridLayoutBg.svg')] bg-cover bg-scroll border-[4px] border-solid border-[#6966ff] rounded-[1.5em] absolute inset-0
+     "
+    ></div>
+  );
+}
+
+export default Layout;
