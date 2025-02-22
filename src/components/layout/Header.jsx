b05e6488a5419362672b49670408caf1fe3ee413
nikiiigoyal <nikiiigoyal@gmail.com>
2025-02-22 21:54:24 +0530

    Made chnages in design of header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,16 +1,31 @@
+import { Button } from "../ui/button";
+
 function Header() {
   return (
     <>
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center font-[16px] m-[25px] ">
         <div className="">
           <img src="/Logo.png" alt="logo"></img>
         </div>
-        <div className="flex justify-around">
-          <a href="/discover-initiatives">Discover initiatives</a>
-          <a href="/funded-grants">Funded grantss</a>
-          <a href="/program-expenses">Program expenes</a>
-          <a href="/blog">Blog</a>
-          <a href="/faq">Faq</a>
+        <div className="flex justify-around p-1 font-['Space_Mono'] text-[16px] items-center">
+          <a href="/discover-initiatives" className="mr-[25px]">
+            Discover initiatives
+          </a>
+          <a href="/funded-grants" className="mr-[25px]">
+            Funded grantss
+          </a>
+          <a href="/program-expenses" className="mr-[25px]">
+            Program expenes
+          </a>
+          <a href="/blog" className="mr-[25px]">
+            Blog
+          </a>
+          <a href="/faq" className="mr-[25px]">
+            Faq
+          </a>
+          <Button className="bg-[#6966FF] text-white cursor-pointer font-serif text-center px-[16px] py-[12px] text-[16px]">
+            Apply for Grant
+          </Button>
         </div>
       </div>
     </>
