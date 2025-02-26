5f27a910d31de04a7302fca4db18537b6e8b2449
nikiiigoyal <nikiiigoyal@gmail.com>
2025-02-26 12:13:08 +0530

    Add: Faq to homepage index page

--- src/components/homePage/Faq.jsx
+++ src/components/homePage/Faq.jsx
@@ -1,24 +1,25 @@
+import AccordionFaq from "./Accordion";
+
 export default function Faq() {
   return (
     <>
-      <div>
-        <div className="flex items-center flex-col mx-5">
-          <h1 className="text-3xl text-[#FFFFFF] mb-5 flex">
+      <div className="mb-8 p-9">
+        <div className="flex items-center flex-col mx-auto w-[30%]">
+          <h1 className="text-4xl text-[#FFFFFF] font-bold mb-5 flex ">
             Frequently asked{" "}
-            <span className="flex">
-              Questions{" "}
-              <img
-                src="/oval.svg"
-                className="absolute right-[-30px] bottom-[-5px] left-auto"
-              ></img>
+            <span className="flex ml-4">
+              questions{"  "}
+              <img src="/oval.svg" className="absolute left-auto"></img>
             </span>
           </h1>
-          <p className="text-[#C8C7D8] text-lg text-center">
+          <p className="text-[#C8C7D8] text-lg text-center mt-9 mb-9 loading-[1.3em]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
             nulla risus, consectetur ut bibendum non, gravida non libero.
           </p>
         </div>
-        <div></div>
+        <div>
+          <AccordionFaq />
+        </div>
       </div>
     </>
   );
