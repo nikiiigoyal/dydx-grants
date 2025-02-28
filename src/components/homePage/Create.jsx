6351371180d1725d46c32b0dcc3b67fb0de128a8
nikiiigoyal <nikiiigoyal@gmail.com>
2025-02-28 13:19:06 +0530

    Modified style of create

--- src/components/homePage/Create.jsx
+++ src/components/homePage/Create.jsx
@@ -3,15 +3,15 @@ import { Button } from "../ui/button";
 export default function Create() {
   return (
     <>
-      <div>
-        <div className="bg-gradient-to-t from-[#6966FF] to-[#6966FF5C] flex flex-col justify-between items-center mx-auto max-w-[1232px] px-[24px] my-9 rounded-2xl">
+      <div className="p-15">
+        <div className="bg-gradient-to-t from-[#6966FF] to-[#6966FF5C] flex flex-col justify-between items-center mx-auto max-w-[1232px] px-9 my-9 rounded-2xl">
           <div>
             <p className="text-[#FFFFFF] text-[18px] p-4 ">
               Have a project in mind?
             </p>
           </div>
           <div>
-            <h1 className="text-3xl text-[#FFFFFF] p-4 mb-4 font-semibold ">
+            <h1 className="text-3xl text-[#FFFFFF] p-4 mb-4 font-semibold  ">
               Let’s create something awesome.
             </h1>
           </div>
