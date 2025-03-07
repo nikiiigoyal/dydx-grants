40a190835865c430bfc95552451769b2f51d1e55
nikiiigoyal <nikiiigoyal@gmail.com>
2025-03-07 17:46:35 +0530

    Add new route in app for Faq

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Layout from "./components/layout";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./components/homePage/index.jsx";
+import FrequentlyAskedQues from "./components/Faq";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Layout>
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="Faq" element={<FrequentlyAskedQues />} />
           </Routes>
         </Layout>
       </BrowserRouter>
