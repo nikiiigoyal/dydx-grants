05ea46ac9a5db67b26e81d08e9010ebb85e15ef5
nikiiigoyal <nikiiigoyal@gmail.com>
2025-03-07 23:06:31 +0530

    Modify Faqs page

--- src/components/Faq/Faqs.jsx
+++ src/components/Faq/Faqs.jsx
@@ -9,12 +9,12 @@ export default function AccordionSelectionCriteria() {
   return (
     <>
       <div>
-        <Accordion type="single" collapsible className="w-[1000px] mx-auto">
-          <AccordionItem value="item-1">
-            <AccordionTrigger className="text-white text-[18px] mb-3 border-b border-t border-[#393953] data-[state=open]:border-b-0 hover:outline-none">
+        <Accordion type="single" collapsible className="w-[1000px] mx-auto ">
+          <AccordionItem value="item-1" className="text-[18px] font-semibold">
+            <AccordionTrigger className=" text-[18px] font-semibold mb-3 border-b border-t border-[#393953] data-[state=open]:border-b-0 hover:outline-none">
               What is the dYdX Grants Trust?
             </AccordionTrigger>
-            <AccordionContent className="pb-[24px] text-lg text-white">
+            <AccordionContent className="pb-[24px] text-lg">
               The dYdX Grants Trust is a Guernsey Purpose Trust was established
               to provide a legal wrapper around the Grants program and
               multi-sig, allowing for the signers to act as Trustees and DYDX
@@ -23,33 +23,44 @@ export default function AccordionSelectionCriteria() {
               to Trustees.
               <br />
               <br />
-              <p>Trust Agreement: </p>
-              <a
-                href="https://drive.google.com/file/d/1HV97VtmeHSt2Fof920TzR7utuSStBFhZ/view"
-                className="text-[#6966ff]"
-              >
-                https://drive.google.com/file/d/1HV97VtmeHSt2F
-                <br />
-                <p> to the number of Trustees required can be found </p>
-              </a>
-              <a href="https://drive.google.com/file/d/1p-uK6lazfr-zF1DBZFAWqhlKZSGx5g7C/view?usp=sharing">
-                here
-              </a>
-              <p>More information here:</p>
-              <a
-                href="https://dydx.foundation/blog/legal-framework-non-us-trusts-in-daos "
-                target="blank"
-              >
-                https://dydx.foundation/blog/legal-framework-non-us-trusts-in-daos
-              </a>
+              <p>
+                Trust Agreement:
+                <a
+                  href="https://drive.google.com/file/d/1HV97VtmeHSt2Fof920TzR7utuSStBFhZ/view"
+                  className="text-[#6966ff]"
+                >
+                  https://drive.google.com/file/d/1HV97VtmeHSt2F{" "}
+                </a>
+              </p>
+              <br />
+              <p>
+                Amendment to the number of Trustees required can be found
+                <a
+                  href="https://drive.google.com/file/d/1p-uK6lazfr-zF1DBZFAWqhlKZSGx5g7C/view?usp=sharing"
+                  className="text-[#6966ff]"
+                >
+                  here
+                </a>
+              </p>
+              <br />
+              <p>
+                More information here:
+                <a
+                  href="https://dydx.foundation/blog/legal-framework-non-us-trusts-in-daos "
+                  target="blank"
+                  className="text-[#6966ff]"
+                >
+                  https://dydx.foundation/blog/legal-framework-non-us-trusts-in-daos
+                </a>
+              </p>
               <br />
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-2">
-            <AccordionTrigger className="text-white text-[18px] mb-3 border-b  border-[#393953] data-[state=open]:border-b-0 underline-0">
+            <AccordionTrigger className="mb-3 border-b  border-[#393953] data-[state=open]:border-b-0 underline-0 text-[18px] font-bold ">
               Where are funds held?
             </AccordionTrigger>
-            <AccordionContent className="pb-[24px] text-lg text-white">
+            <AccordionContent className="pb-[24px] text-lg">
               Funds are held in a Gnosis safe managed by the 5 Trustees.
               <br></br>
               <p>
@@ -65,11 +76,11 @@ export default function AccordionSelectionCriteria() {
               </p>
             </AccordionContent>
           </AccordionItem>
-          <AccordionItem value="item-3">
-            <AccordionTrigger className="text-white text-[18px] mb-3 border-b  border-[#393953] data-[state=open]:border-b-0 underline-0">
+          <AccordionItem value="item-3" className="text-[18px] font-semibold">
+            <AccordionTrigger className="text-[18px] font-bold mb-3 border-b  border-[#393953] data-[state=open]:border-b-0 underline-0">
               When was the program launched?
             </AccordionTrigger>
-            <AccordionContent className="pb-[24px] text-lg text-white">
+            <AccordionContent className="pb-[24px] text-lg">
               The program officially launched in January 2022 after being
               approved by the community through a governance proposal.
               <br></br>
@@ -86,11 +97,11 @@ export default function AccordionSelectionCriteria() {
               </p>
             </AccordionContent>
           </AccordionItem>
-          <AccordionItem value="item-4">
-            <AccordionTrigger className="text-white text-[18px] mb-3 border-b  border-[#393953] data-[state=open]:border-b-0 underline-0">
+          <AccordionItem value="item-4" className="text-[18px] font-semibold">
+            <AccordionTrigger className="text-[18px] font-bold mb-3 border-b  border-[#393953] data-[state=open]:border-b-0 underline-0">
               Who are the Trustees?
             </AccordionTrigger>
-            <AccordionContent className="pb-[24px] text-lg text-white">
+            <AccordionContent className="pb-[24px] text-lg">
               Trustees are responsible for carrying out the purpose of the
               Trust. We have the following Trustees:
               <br></br>
@@ -115,7 +126,6 @@ export default function AccordionSelectionCriteria() {
                   here
                 </a>
               </p>
-              <br />
               <p>
                 The second amendment to Trustees executed in Grants v1.5 renewal
                 can be found{" "}
@@ -126,7 +136,6 @@ export default function AccordionSelectionCriteria() {
                   here
                 </a>
               </p>
-              <br />
               <p>
                 The third amendment to Trustees executed in Grants v1.5 renewal
                 can be found{" "}
@@ -139,11 +148,11 @@ export default function AccordionSelectionCriteria() {
               </p>
             </AccordionContent>
           </AccordionItem>
-          <AccordionItem value="item-5">
-            <AccordionTrigger className="text-white text-[18px] mb-3 border-b  border-[#393953] data-[state=open]:border-b-0 underline-0">
+          <AccordionItem value="item-5" className="text-[18px] font-semibold">
+            <AccordionTrigger className="text-[18px] font-bold mb-3 border-b  border-[#393953] data-[state=open]:border-b-0 underline-0">
               What and who is the Enforcer?
             </AccordionTrigger>
-            <AccordionContent className="pb-[24px] text-lg text-white">
+            <AccordionContent className="pb-[24px] text-lg">
               The Enforcer is responsible for ensuring Trustees carry out the
               purpose of the Trust and act in the best interests of the
               beneficiaries.
