17369f54bc4df02dfa1fd78b9eb4221e12137508
nikiiigoyal <nikiiigoyal@gmail.com>
2025-03-08 20:41:24 +0530

    Modified design of faq page

--- src/components/Faq/index.jsx
+++ src/components/Faq/index.jsx
@@ -1,11 +1,12 @@
 import AccordionFaq from "../homePage/Accordion";
+import Create from "../homePage/Create";
 import AccordionSelectionCriteria from "./Faqs";
 
 function FrequentlyAskedQues() {
   return (
     <>
       <div
-        className="flex flex-col justify-around items-center text-[#c8c7d8] tracking-[-.022em] cursor-pointer text-[18px] font-semibold
+        className="flex flex-col justify-around items-center text-[#c8c7d8] tracking-[-.022em] cursor-pointer 
        "
       >
         <h1 className="text-[40px] tracking-[-2.2%] font-bold text-center font-circular-std leading-[72px] text-white mt-6">
@@ -20,11 +21,20 @@ function FrequentlyAskedQues() {
           </div>
           <div>
             <h2 className="leading-[32px] tracking-[-1.9%] text-2xl text-white font-bold my-8">
+              Selection Criteria
+            </h2>
+            <AccordionSelectionCriteria />
+          </div>
+          <div>
+            <h2 className="leading-[32px] tracking-[-1.9%] text-2xl text-white font-bold my-8">
               The Grants Trust
             </h2>
             <AccordionSelectionCriteria />
           </div>
         </div>
+      </div>
+      <div>
+        <Create />
       </div>
     </>
   );
