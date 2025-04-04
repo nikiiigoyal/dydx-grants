658ac9c831790b7b76c8cb665885f624a75a7de6
nikiiigoyal <nikiiigoyal@gmail.com>
2025-04-04 18:08:14 +0530

    Modified form

--- src/components/applyForGrants/index.jsx
+++ src/components/applyForGrants/index.jsx
@@ -4,10 +4,17 @@ function ApplyForGrants() {
   return (
     <>
       <div>
-        <h1 className="text-3xl">Apply for a grant</h1>
-        <p>Please complete the application below for Grant consideration!</p>
+        <h1 className="text-4xl font-bold text-[#FFFFFF] text-center mb-5">
+          Apply for a grant
+        </h1>
+        <p className="text-sm text-[#C8C7D8] text-center">
+          Please complete the application below for Grant consideration!
+        </p>
       </div>
-      <div>
+      <div className=" mt-5">
+        {/* <h1 className="text-[#FFFFFF] text-2xl font-semibold">
+          Grant Application
+        </h1> */}
         <Form />
       </div>
     </>
