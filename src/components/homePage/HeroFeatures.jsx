f135046d8bfbe75682c16614316f3aacd3e8cb79
nikiiigoyal <nikiiigoyal@gmail.com>
2025-04-10 19:50:49 +0530

    made features section responsive

--- src/components/homePage/HeroFeatures.jsx
+++ src/components/homePage/HeroFeatures.jsx
@@ -1,24 +1,18 @@
 function Features() {
   return (
     <>
-      <div className="font-['Space_Mono'] flex justify-around mt-10 p-5">
-        <div>
-          <div className="text-[#FFFFFF] text-[20px] text-center">100+ </div>
-          <div className="text-[#C8C7D8] text-[18px] text-center">
-            projects funded
-          </div>
+      <div className="font-['Space_Mono'] flex flex-col md:flex-row justify-around mt-10 p-5 gap-8 md:gap-0">
+        <div className="text-center">
+          <div className="text-white text-xl">100+ </div>
+          <div className="text-[#C8C7D8] text-base">projects funded</div>
         </div>
-        <div>
-          <div className="text-[#FFFFFF] font-[20px] text-center">
-            $3+ milion
-          </div>
-          <div className="text-[#C8C7D8] text-[18px] text-center">awarded</div>
+        <div className="text-center">
+          <div className="text-white text-xl">$3+ milion</div>
+          <div className="text-[#C8C7D8] text-base">awarded</div>
         </div>
-        <div>
-          <div className="text-[#FFFFFF] font-[20px] text-center">400+ </div>
-          <div className="text-[#C8C7D8] text-[18px] text-center">
-            projects funded
-          </div>
+        <div className="text-center">
+          <div className="text-white text-xl">400+ </div>
+          <div className="text-[#C8C7D8] text-base">projects funded</div>
         </div>
       </div>
     </>
