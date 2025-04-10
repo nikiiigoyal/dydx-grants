e7b0c132084b661218a8ca85c6b61d72de5f292d
nikiiigoyal <nikiiigoyal@gmail.com>
2025-04-10 20:04:45 +0530

    Made hero section of homepage responsive

--- src/components/homePage/Hero.jsx
+++ src/components/homePage/Hero.jsx
@@ -2,13 +2,11 @@ import { Button } from "../ui/button";
 import Features from "./HeroFeatures";
 
 export default function Hero() {
-  console.log("Hero component rendered!");
   return (
     <>
-      <div className="w-[50%] items-center mx-auto font-['Circular',_sans-serif]">
-        <div className="text-[80px] text-center text-[#FFFFFF] font-bold flex flex-col ">
+      <div className="w-full md:w-4/5 lg:w-3/5 items-center mx-auto font-['Circular',_sans-serif] px-4">
+        <div className="text-3xl sm:text-5xl md:text-6xl text-center text-[#FFFFFF] font-bold flex flex-col">
           <h1 className="inline">Powering the future of dYdX through</h1>
-          {/* <h1 className="inline">dYdX through community</h1> */}
 
           <div>
             <h1 className="relative inline-block">
@@ -18,16 +16,16 @@ export default function Hero() {
             <img
               src="/lineHero.svg"
               alt="line"
-              className="absolute right-140"
+              className="absolute right-0 md:right-140 max-w-full"
             ></img>
           </div>
         </div>
-        <div className="flex justify-around w-[50%] mx-auto mt-6 min-w-[180px]">
-          <Button className="bg-[#393953] text-white text-center px-[30px] py-[27px] font-['Circular',_sans-serif] text-[18px] hover:transform hover:scale-105 hover:bg-[#4f4c64]">
+        <div className="flex justify-around w-full flex-col sm:flex-row mx-auto my-8 gap-4 sm:w-4/5 md:w-3/4 lg:w-2/3">
+          <Button className="w-full sm:w-auto bg-[#393953] text-white text-center px-[30px] py-[27px] font-['Circular',_sans-serif] text-[18px] hover:transform hover:scale-105 hover:bg-[#4f4c64]">
             Discover REPs
           </Button>
-          <div className="flex">
-            <Button className="font-['Circular',_sans-serif] text-[18px] px-[30px] py-[27px] hover:transform hover:scale-105">
+          <div className="w-full sm:w-auto">
+            <Button className="w-full sm:w-auto font-['Circular',_sans-serif] text-[18px] px-[30px] py-[27px] hover:transform hover:scale-105">
               Apply for grant
               <img src="/arrow_right.svg"></img>
             </Button>
