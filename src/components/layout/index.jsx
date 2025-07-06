e88c0f5d54308ad5d06bbe4fff09894332678759
nikiiigoyal <nikiiigoyal@gmail.com>
2025-07-06 11:01:19 +0530

    hide overflow

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -4,20 +4,18 @@ import Footer from "./Footer";
 
 function Layout({ children }) {
   return (
-    <div
-    //   className="bg-[#1c1c28] bg-[url('/gridLayoutBg.svg')] bg-scroll bg-cover border-[4px] border-solid border-[#6966ff] rounded-[1.5em] absolute inset-0 bg-repeat
-    //  "
-    >
+    <div className="min-h-screen flex flex-col w-full overflow-x-hidden bg-[#1c1c28] bg-[url('/gridLayoutBg.svg')] bg-scroll bg-cover border-[4px] border-solid border-[#6966ff] rounded-[1.5em] bg-repeat">
       <Header />
-      <div className="overflow-y-auto h-full">{children}</div>
+      <main className="flex-1 w-full overflow-x-hidden px-4 py-6 md:px-6 lg:px-8">
+        {children}
+      </main>
       <Footer />
     </div>
   );
 }
+
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-
-  // className: PropTypes.string,
 };
 
 export default Layout;
